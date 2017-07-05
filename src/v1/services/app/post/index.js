142fea79c347b0c075e9e41fd42ebdb92c3dcc01
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-07-04 21:15:59 -0300

    Implementado get post. Adicionado informação do autor nas funções que buscam posts.

--- src/v1/services/app/post/index.js
+++ src/v1/services/app/post/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import validate from 'express-validation';
 
-import { createPost, updatePost, getPosts } from '../../../models/app/post/';
-import { createPostSchema, updatePostSchema, getPostsSchema } from './schema';
+import { createPost, updatePost, getPosts, getPost } from '../../../models/app/post/';
+import { createPostSchema, updatePostSchema, getPostsSchema, getPostSchema } from './schema';
 
 const router = express.Router();
 
 router.post('/', validate(createPostSchema), ({ body, user }, res, next) => {
   createPost({ ...body, ownerId: user._id })
     .then(payload => res.status(201).json({ payload }))
+    .catch(error => next(error));
+});
+
+router.get('/:postId', validate(getPostSchema), ({ params }, res, next) => {
+  const { postId } = params;
+  getPost(postId)
+    .then(payload => res.status(200).json({ payload }))
     .catch(error => next(error));
 });
 
